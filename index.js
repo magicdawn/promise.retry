749b0958881b2336c3b48f8d581f93942f814929
magicdawn@qq.com <magicdawn@qq.com>
2016-05-15 10:22:32 +0800

    code: add

---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+/**
+ * module dependencies
+ */
+
+const co = require('co');
+const ptimeout = require('promise.timeout');
+const TimeoutError = ptimeout.TimeoutError;
+const inherits = require('util').inherits;
+
+/**
+ * main fn
+ */
+
+module.exports = function promiseRetry(fn, options) {
+
+  // fn 备份
+  const originalFn = fn;
+
+  // 重试次数
+  const times = options.times || 5; // 5 次
+
+  // 超时
+  const _timeout = options.timeout || false;
+  if (_timeout) fn = ptimeout(fn, _timeout);
+
+  // 额外错误处理
+  const onerror = options.onerror;
+
+
+  return co.wrap(function*() {
+    const ctx = this;
+    const args = [].slice.call(arguments);
+    const errors = new Array(times);
+
+    for (let i = 0; i < times; i++) {
+      let result, err;
+      try {
+        result = yield fn.apply(ctx, args);
+      } catch (e) {
+        err = e;
+      }
+
+      if (err) {
+        errors[i] = err;
+
+        // 额外错误处理
+        if (onerror) {
+          onerror(err, i); // err, current attempt, wait ?
+        }
+
+        continue;
+      } else {
+        return result;
+      }
+    }
+
+    throw new RetryError({
+      times: times,
+      timeout: _timeout,
+      fn: originalFn,
+      errors: errors // 错误列表
+    });
+  });
+};
+
+function RetryError(times, timeout, fn) {
+  Error.call(this);
+
+  // props
+  this.times = times;
+  this.timeout = timeout;
+  this.fn = fn;
+
+  // message
+  this.message = `tried function ${ fn.name || '<anonymous>' } ${ times } times with timeout = ${ timeout }ms`;
+
+  // stack
+  Error.captureStackTrace(this, RetryError);
+}
+
+inherits(RetryError, Error);
+
+/**
+ * exports Error class
+ */
+
+module.exports.RetryError = RetryError;
+module.exports.TimeoutError = ptimeout.TimeoutError;
