44a71606ac7232135ecfed1dde332a5eb63e01f0
magicdawn <magicdawn@qq.com>
2022-11-11 16:07:35 +0800

    dts: use export =

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,26 +1,32 @@
-import { TimeoutError, EnsurePromise } from 'promise.timeout'
-export { TimeoutError, EnsurePromise }
+import { TimeoutError } from 'promise.timeout'
 
-export class RetryError extends Error {
+declare type RetryOptions = {
+  times?: number
+  timeout?: number
+  onerror?: (err: Error, index: number) => void
+}
+
+declare class RetryError extends Error {
   times: number
   timeout: number
   fn: (...args: any[]) => any
   errors: Error[]
 }
 
-export type RetryOptions = {
-  times?: number
-  timeout?: number
-  onerror?: (err: Error, index: number) => void
-}
-
 // with signal
-export default function pretry<T extends unknown[], R>(
+declare function pretry<T extends unknown[], R>(
   fn: (...args: [...args: T, signal?: AbortSignal]) => R,
   options?: RetryOptions
-): (...args: T) => EnsurePromise<R>
+): (...args: T) => Promise<Awaited<R>>
+
 // without signal
-export default function pretry<T extends unknown[], R>(
+declare function pretry<T extends unknown[], R>(
   fn: (...args: [...args: T]) => R,
   options?: RetryOptions
-): (...args: T) => EnsurePromise<R>
+): (...args: T) => Promise<Awaited<R>>
+
+declare namespace pretry {
+  export { TimeoutError, RetryError, RetryOptions }
+}
+
+export = pretry
